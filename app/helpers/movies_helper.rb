dfb5194825b01a70597c061e2aef8ca54ceec4e8
wesleymsan <wesleymsan14@gmail.com>
2021-02-01 07:49:36 -0300

    Add view helpers

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,15 @@
+# frozen_string_literal: true
+
 module MoviesHelper
+  def total_gross(movie)
+    if movie.flop?
+      'Flop!'
+    else
+      number_to_currency(movie.total_gross, precision: 0)
+    end
+  end
+
+  def year_of(movie)
+    movie.released_on.year
+  end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
-# Movie model
+# Movie
 class Movie < ApplicationRecord
+  def flop?
+    total_gross.blank? || total_gross < 250_000_000
+  end
 end
