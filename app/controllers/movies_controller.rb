75ef1f07c08b513004fea34e5ff7dfd1845be1ed
wesleymsan <wesleymsan14@gmail.com>
2021-02-05 13:03:39 -0300

    Add forms: editing and creating

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,6 +10,20 @@ class MoviesController < ApplicationController
     @movie = Movie.find params[:id]
   end
 
-  def root
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find params[:id]
+
+    @movie.update(movie_params)
+
+    redirect_to @movie
+  end
+
+  def movie_params
+    @movie = params.require(:movie)
+    .permit(:title, :description, :rating, :released_on, :total_gross)
   end
 end
