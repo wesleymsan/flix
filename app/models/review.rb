16c5537f1dafacdc2faa2c39f898ab25fc44c088
wesleymsan <wesleymsan14@gmail.com>
2021-02-17 21:27:33 -0300

    Add the one-to-many: nested resources and forms

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,10 +3,16 @@ class Review < ApplicationRecord
 
   validates :name, presence: true
 
-  validates :comment, length: { minimum: 5 }
+  validates :comment, length: { minimum: 4 }
 
-  STARS = %w[1 2 3 4 5].freeze
+  STARS = [1, 2, 3, 4, 5].freeze
+
   validates :stars, inclusion: {
-    in: STARS, message: 'must be between 1 and 5'
+    in: STARS,
+    message: 'must be between 1 and 5'
   }
+
+  def stars_as_percent
+    (stars / 5.0) * 100.0
+  end
 end
