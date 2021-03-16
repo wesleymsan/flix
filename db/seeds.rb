07d8c76b93ebb733f7eff4d881266855855f7c07
Wesley Santos <wesley.santos@aplicativo.net>
2021-03-15 23:39:24 -0300

    Update Ruby version for run in Windows system, and Add functionality to delete reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Movie.create!
-[
+Movie.create!([
   {
     title: 'Avengers: Endgame',
     description:
@@ -174,4 +173,4 @@ Movie.create!
     duration: "141 min",
     image_file_name: "wonder-woman.png"
   }
-]
+])
