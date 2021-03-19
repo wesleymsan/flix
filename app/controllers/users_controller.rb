5eab6c50aeea7e16cd50fe4cf9fbc27998c04b35
Wesley Santos <wesley.santos@aplicativo.net>
2021-03-18 21:01:45 -0300

    Add user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,48 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user, notice: 'User successfully updated!'
+    else
+      render :edit
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user, notice: 'Thanks for signing up!'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @user = User.find params[:id]
+    @user.destroy
+    redirect_to users_url, alert: 'User successfully deleted!'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).
+      permit(:name, :email, :password, :password_confirmation)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users
+  get "signup" => "users#new"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'movies#index'
   resources :movies do
